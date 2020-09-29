45fee4b186524594a9d0bee22add5a95f6f67911
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-29 10:54:49 -0300

    Slider

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+          padding: EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: [
+              _crearSlider(),
+              Expanded(child: _crearImagen()),
+            ],
+          )),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de la imagen',
+      value: _valorSlider,
+      min: 10,
+      max: 400,
+      onChanged: (valor) {
+        setState(() {
+          _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://static1.squarespace.com/static/56a5f5ef5827c35bf58e8e02/t/5a7042a30d9297dd2e320096/1517306532240/Algo_Logo.jpeg?format=1500w'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
